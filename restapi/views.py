8a27cc99de291e7c7e75f16272063440937164ea
sachin-kumar-m <sachin.kumar.maheshwarappa@gmail.com>
2022-05-09 15:42:24 +0530

    fix for view.py

--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,10 @@
+from django import views
+from django.http import HttpResponse
 from django.shortcuts import render
+from views import views
 
 # Create your views here.
+
+class NewArticle(views):
+    def get(self, request):
+        return render (HttpResponse("This is a view"))
