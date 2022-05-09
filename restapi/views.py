734c467e8c418d2af8d8e41f70cec3f8a7e57420
sachin-kumar-m <sachin.kumar.maheshwarappa@gmail.com>
2022-05-09 19:16:52 +0530

    added new class in the views

--- restapi/views.py
+++ restapi/views.py
@@ -8,3 +8,5 @@ from views import views
 class NewArticle(views):
     def get(self, request):
         return render (HttpResponse("This is a view"))
+    def post(self, request):
+        return HttpResponse("this is a simple message")
